e2a9ec7697f6c4111e9951a58653fe56ba40d126
andrahafiz <andra18ti@mahasiswa.pcr.ac.id>
2020-03-10 15:07:32 +0700

    Initial commit

---
+++ Program/BelajarC/BelajarC/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelajarC
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            HaloTeman(0);
+            HaloTeman(2, "Ani", "Toni", "Budi");
+            Console.ReadKey();
+        }   
+
+        static void HaloTeman(int parameterTambahan, params string[] names)
+        {
+            foreach (string name in names)
+                Console.WriteLine("Halo, " + name);
+        }
+
+    }
+}
